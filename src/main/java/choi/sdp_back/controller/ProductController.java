62d4ff90ad778c6b4eb7f8941e2bcae477b2f4ca
choi <a01037307212@gmail.com>
2025-12-29 22:41:08 +0900

    image and product

--- src/main/java/choi/sdp_back/controller/ProductController.java
+++ src/main/java/choi/sdp_back/controller/ProductController.java
@@ -3,16 +3,6 @@ package choi.sdp_back.controller;
 import choi.sdp_back.dto.ProductDto;
 import choi.sdp_back.service.ProductService;
 import lombok.RequiredArgsConstructor;
-<<<<<<< HEAD
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/products")
-=======
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,32 +15,10 @@ import java.util.List;
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000") // CORS 개별 허용
->>>>>>> 6ad3a46f4f4624220ae986c885a40afbdb246f8f
 public class ProductController {
 
     private final ProductService productService;
 
-<<<<<<< HEAD
-    // 모든 제품 조회
-    @GetMapping
-    public List<ProductDto> getAllProducts() {
-        return productService.getAllProducts();
-    }
-
-    // 새 제품 생성
-    @PostMapping
-    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto) {
-        return ResponseEntity.ok(productService.createProduct(productDto));
-    }
-
-    // 제품 수정
-    @PutMapping("/{id}")
-    public ResponseEntity<ProductDto> updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
-        return ResponseEntity.ok(productService.updateProduct(id, productDto));
-    }
-
-    // 제품 삭제
-=======
     @GetMapping
     public ResponseEntity<List<ProductDto>> getAllProducts() {
         return ResponseEntity.ok(productService.getAllProducts());
@@ -71,7 +39,6 @@ public class ProductController {
         return ResponseEntity.ok(productService.updateProduct(id, productDto, image));
     }
 
->>>>>>> 6ad3a46f4f4624220ae986c885a40afbdb246f8f
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
